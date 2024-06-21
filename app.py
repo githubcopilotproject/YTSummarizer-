21168830cdee690dfb48c8ae14d3b116997feb5f
githubcopilotproject <137425420+githubcopilotproject@users.noreply.github.com>
2024-06-21 17:03:27 +0530

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,8 @@ from langchain_community.vectorstores import FAISS
 from langchain.chains.question_answering import load_qa_chain
 from langchain.prompts import PromptTemplate
 from langchain_google_genai import GoogleGenerativeAIEmbeddings
-embeddings = GoogleGenerativeAIEmbeddings(model = "models/embedding-001", google_api_key=os.getenv("GOOGLE_API_KEY"))
+#embeddings = GoogleGenerativeAIEmbeddings(model = "models/embedding-001", google_api_key=st.secrets["GOOGLE_API_KEY"])
+embeddings = GoogleGenerativeAIEmbeddings(google_api_key=st.secrets["GOOGLE_API_KEY"])
 from langchain_google_genai import (ChatGoogleGenerativeAI, HarmBlockThreshold, HarmCategory)
 
 from youtube_transcript_api import YouTubeTranscriptApi
